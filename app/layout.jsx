fe9c4c33a83ca827b3a3c817ecd1f0e4ddee08ea
suresh <sureshnani86@gmail.com>
2023-05-20 08:51:25 +0530

    Intial Setup

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Prompto",
+  description: "Discover & Share AI Prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
